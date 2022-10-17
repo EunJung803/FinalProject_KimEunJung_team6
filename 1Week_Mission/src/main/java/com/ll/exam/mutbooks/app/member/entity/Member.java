78805cc0fd3e565e86df4de10fe12d52da161acc
EunJung803 <aimee3322@gmail.com>
2022-10-17 16:52:05 +0900

    Feat : 회원가입, 로그인 구현 layout 적용 (수정 필요)

---
+++ 1Week_Mission/src/main/java/com/ll/exam/mutbooks/app/member/entity/Member.java
@@ -0,0 +1,35 @@
+package com.ll.exam.mutbooks.app.member.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ll.exam.mutbooks.app.base.entity.BaseEntity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
+    @Column(unique = true)
+    private String username;
+    @JsonIgnore
+    private String password;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String nickname;
+
+    public String getName() {
+        return username;
+    }
+
+    public Member(long id) {
+        super(id);
+    }
+}
