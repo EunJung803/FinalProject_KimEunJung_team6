a2aa1996bb556fd73143e8c36398d117ffa2b599
EunJung803 <aimee3322@gmail.com>
2022-10-17 12:02:26 +0900

    setting

---
+++ 1Week_Mission/src/main/java/com/ll/exam/mutbooks/MutBooksApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.mutbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutBooksApplication.class, args);
+    }
+
+}
---
+++ 1Week_Mission/src/test/java/com/ll/exam/mutbooks/MutBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.mutbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
