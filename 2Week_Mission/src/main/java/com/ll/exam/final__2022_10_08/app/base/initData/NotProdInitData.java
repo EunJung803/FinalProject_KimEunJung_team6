d164d0532d051a1e56c6b8a6174a48bafa54ff57
EunJung803 <aimee3322@gmail.com>
2022-10-28 03:07:35 +0900

    Feat : 결제 완료 이후 MyBook 테이블에 구매한 책 추가됨

--- 2Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/base/initData/NotProdInitData.java
+++ 2Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/base/initData/NotProdInitData.java
@@ -97,7 +97,7 @@ public class NotProdInitData {
             memberService.addCash(member1, 10_000, "충전__무통장입금");
             memberService.addCash(member1, 20_000, "충전__무통장입금");
             memberService.addCash(member1, -5_000, "출금__일반");
-//            memberService.addCash(member1, 1_000_000, "충전__무통장입금");
+            memberService.addCash(member1, 1_000_000, "충전__무통장입금");
 //
 //            memberService.addCash(member2, 2_000_000, "충전__무통장입금");
 
