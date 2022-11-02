7a8df7bf9149447ba61563d2368999f588daf040
EunJung803 <aimee3322@gmail.com>
2022-11-02 15:43:45 +0900

    Feat : rebateOrderItemList 구현

--- 3Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/order/service/OrderService.java
+++ 3Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/order/service/OrderService.java
@@ -144,7 +144,7 @@ public class OrderService {
         orderRepository.delete(order);
     }
 
-    public List<OrderItem> findAllByPayDateBetween(LocalDateTime fromDate, LocalDateTime toDate) {
+    public List<OrderItem> findAllByPayDateBetweenOrderByIdAsc(LocalDateTime fromDate, LocalDateTime toDate) {
         return orderItemRepository.findAllByPayDateBetween(fromDate, toDate);
     }
 }
--- 3Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/rebate/controller/AdmRebateController.java
+++ 3Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/rebate/controller/AdmRebateController.java
@@ -1,16 +1,20 @@
 package com.ll.exam.final__2022_10_08.app.rebate.controller;
 
+import com.ll.exam.final__2022_10_08.app.base.rq.Rq;
+import com.ll.exam.final__2022_10_08.app.rebate.entity.RebateOrderItem;
 import com.ll.exam.final__2022_10_08.app.rebate.service.RebateService;
 import com.ll.exam.final__2022_10_08.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 @RequestMapping("/adm/rebate")
@@ -26,11 +30,25 @@ public class AdmRebateController {
 
     @PostMapping("/makeData")
     @PreAuthorize("hasAuthority('ADMIN')")
-    @ResponseBody
     public String makeData(String yearMonth) {
 
         rebateService.makeDate(yearMonth);
 
-        return "성공";
+//        return "redirect:/adm/rebate/rebateOrderItemList?yearMonth=" + yearMonth + "&msg=" + Ut.url.encode("정산데이터가 성공적으로 생성되었습니다.");
+        return Rq.redirectWithMsg("/adm/rebate/rebateOrderItemList", "%s월 정산데이터가 성공적으로 생성되었습니다.".formatted(yearMonth));
+    }
+
+    @GetMapping("/rebateOrderItemList")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String showRebateOrderItemList(String yearMonth, Model model) {
+        if (yearMonth == null) {
+            yearMonth = "2022-10";
+        }
+
+        List<RebateOrderItem> items = rebateService.findRebateOrderItemsByPayDateIn(yearMonth);
+
+        model.addAttribute("items", items);
+
+        return "adm/rebate/rebateOrderItemList";
     }
 }
