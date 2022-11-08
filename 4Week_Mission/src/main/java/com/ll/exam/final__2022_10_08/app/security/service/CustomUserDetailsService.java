144cf57d8b4eff2fe65e1baee4479191575726f4
EunJung803 <aimee3322@gmail.com>
2022-11-08 21:09:47 +0900

    4주차 setting

---
+++ 4Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/security/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.ll.exam.final__2022_10_08.app.security.service;
+
+import com.ll.exam.final__2022_10_08.app.member.entity.Member;
+import com.ll.exam.final__2022_10_08.app.member.repository.MemberRepository;
+import com.ll.exam.final__2022_10_08.app.security.dto.MemberContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username).get();
+
+        if (member.getUsername().equals("user3")) {
+            member.setAuthLevel(7);     // user3을 관리자로 설정
+        }
+
+        return new MemberContext(member, member.genAuthorities());
+    }
+}
